0f5ecba1f80b2d881e316dc98be97f9906c04b23
Marcos Speca <marcos.speca@legalinsights.com.br>
2020-10-15 18:55:44 -0300

    Ajustes

--- application.py
+++ application.py
@@ -16,7 +16,7 @@ def index():
     cnx = connector.connect(user='admin', password='TaTxMe123', host='dbmovies.cthpl6d1wkbm.us-east-1.rds.amazonaws.com', database='dbmovies')
     cursor = cnx.cursor()
     
-    rand_id = randint(1, 100)
+    rand_id = randint(1, 33)
     
     query = ("SELECT * FROM movie_quotes WHERE ID= %s")
     cursor.execute(query, (rand_id,))
@@ -28,8 +28,7 @@ def index():
     page = ("<html><title>49BDT Fiap Trabalho Cloud</title><body align=center>" + 
             "<br><br><br><br><br><br><br><br>" +
             "<h1>" + row[1] + 
-            "</h1><br>" + 
-            row[2] + 
+            "</h1><br><h3><i> --" + row[2] + "</i></h3>" +
             "</body></html>")
 
     return page
